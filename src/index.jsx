50dc4026bf879d851af8d064a3f6984774d520ed
Heritina <randrianjafyheritina7@gmail.com>
2026-08-25 10:26:56 +0300

    push1

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // <-- Indispensable pour que Tailwind fonctionne sous Vite !
+import './index.css' // Crucial pour charger vos styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
